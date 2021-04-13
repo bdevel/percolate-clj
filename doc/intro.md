10c4f95e7c18ed5b148e218c812542089e0e1240
Tyler Roberts <github@polar-concepts.com>
2021-04-13 08:12:11 -0700

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to percolate-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/percolate_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns percolate-clj.core-test
+  (:require [clojure.test :refer :all]
+            [percolate-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
